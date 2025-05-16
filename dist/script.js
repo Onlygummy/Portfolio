30b390c77be0ce7b9a6c441a15c4f9fa792efbd5
Onlygummy <arsik.789.king@gmail.com>
2025-05-17 02:28:47 +0700

    update

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,5 @@
 "use strict";
 //import keyboard_ascii from './ascii-art/keyboard.ts';
-let text = document.getElementById("ascii-keyboard");
+//let text: HTMLTextAreaElement = document.getElementById("ascii-keyboard") as HTMLTextAreaElement;
 //text.innerText = "123456789";
 //console.log(keyboard_ascii[0]);
--- docs/script.ts
+++ docs/script.ts
@@ -1,6 +1,6 @@
 //import keyboard_ascii from './ascii-art/keyboard.ts';
 
-let text: HTMLTextAreaElement = document.getElementById("ascii-keyboard") as HTMLTextAreaElement;
+//let text: HTMLTextAreaElement = document.getElementById("ascii-keyboard") as HTMLTextAreaElement;
 
 //text.innerText = "123456789";
 
